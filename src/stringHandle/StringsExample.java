16309ff1577fc13445bbaf0d371a8295077a74cb
Mahesh <70198087+maheshmaersk@users.noreply.github.com>
2022-07-12 08:57:03 +0530

    Anil String Handling Example

--- src/stringHandle/StringsExample.java
+++ src/stringHandle/StringsExample.java
@@ -17,15 +17,40 @@ public class StringsExample {
 //        System.out.println(a.equals(b));
 //        System.out.println(a.equalsIgnoreCase(c));
 
+//
+//        Scanner acn = new Scanner(System.in);
+//        System.out.println("Enter the First Name");
+//        String op1 = acn.next();
+//        System.out.println("Enter the Last Name");
+//        String op2 = acn.next();
+//
+//        System.out.println("Entered name is "+op1.toUpperCase(Locale.ROOT)+" "+op2.toUpperCase(Locale.ROOT));
+//        System.out.println("Entered name is "+op1.toLowerCase(Locale.ROOT)+" "+op2.toLowerCase(Locale.ROOT));
 
-        Scanner acn = new Scanner(System.in);
-        System.out.println("Enter the First Name");
-        String op1 = acn.next();
-        System.out.println("Enter the Last Name");
-        String op2 = acn.next();
+        String ans = "This is just a warning and need to see how it works";
+        String ans1 = "023-34554545";
 
-        System.out.println("Entered name is "+op1.toUpperCase(Locale.ROOT)+" "+op2.toUpperCase(Locale.ROOT));
+        String[] sdd = ans.split(" ");
+//        for (String sdas:sdd) {
+//            System.out.println(sdas);
+//        }
+        System.out.println(sdd.length);
 
+        for (int i = sdd.length-1; i >= 0; i--) {
+            System.out.print(sdd[i]+" ");
+        }
+        System.out.println();
+        System.out.println(ans.substring(10));
+
+
+        String asmd = " Sankeeth      ";
+
+        System.out.println("Length of String "+asmd.trim().length());
+
+        System.out.println(asmd.trim().startsWith("Sa"));
+        System.out.println(asmd.trim().endsWith("th"));
+        System.out.println(asmd.trim().indexOf("th"));
+        System.out.println(asmd.trim().contains("t"));
 
     }
 }
